18ad7663887938bcb7bf342a11971f9cc45bde1b
SONU KUSHWAHA <116018711+Sonu7804@users.noreply.github.com>
2023-10-13 10:01:40 +0530

    Add files via upload

---
+++ missing.java
@@ -0,0 +1,23 @@
+public class missing {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 7, 6, 5 };
+        int n =arr.length;
+
+        // sum of natural number
+
+        int sum_natural_num = ((n + 1) * (n+2)) / 2;
+        int sum = 0;
+
+        // sum of current elements present in an arrey
+
+        for (int i = 0; i<n; i++) {
+            sum += arr[i];
+        }
+
+        int missing_elem = 0;
+        missing_elem = sum_natural_num - sum;
+
+         System.out.println("Missing elements in an array is:"+missing_elem);
+    }
+}
